d6dacc9b62ccfcfa7e246ebc506c1dfb3371fe7f
arishermawan <arishermawan@hotmail.com>
2019-10-19 21:55:40 +0700

    store state in board components

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
+import { runInThisContext } from 'vm';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
       <button
         className='square'
-        onClick={() => this.setState({ value: 'X' })}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
-      </button>
+        {this.props.value}
+      </button >
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares: squares })
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
